95162b3385f8c63225f9d2d41ff0edfbc5f354f6
no382001 <102482527+no382001@users.noreply.github.com>
2022-04-04 14:42:40 +0000

    Delete back directory

--- back/save.php
+++
@@ -1,9 +0,0 @@
-function printascii($txt_file,$id){
-				$lines = file($txt_file);
-				$linenumber = 0;
-				foreach ($lines as $line){
-					//if the first or last row of the ascii file is not empty, this echo will echo a random amount of \t-s to the front or the back for some reason, messing up the text centering and replacing
-					echo '<p id=row'.$linenumber.$id.'>'.$line."</p>";
-					$linenumber += 1;
-					}
-				}
